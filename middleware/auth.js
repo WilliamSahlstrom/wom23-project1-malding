6f11bd57e26ca56be4004ffd3b5ede7a6c4f6b60
WilliamSahlstrom <sahlstrw@arcada.fi>
2023-09-22 14:01:07 +0300

    Creating new server.js, auth.js and authRoutes.js. Adding an empty file boardRoutes.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,20 +1,35 @@
 const jwt = require('jsonwebtoken');
-require('dotenv').config(); // läs in alla variabler i .env
+require('dotenv').config();
 
 module.exports = (req, res, next) => {
     console.log('auth middleware');
-    try{
-        // plocka ut jwt från headern
-        const token = req.headers['authorization'].split(' ')[1];
-        // Verifiera token och spara användarinfo
+    try {
+        // Check if the Authorization header is present
+        const authHeader = req.headers.authorization;
+        if (!authHeader) {
+            throw new Error('Missing Authorization header');
+        }
+
+        // Check if the Authorization header has the expected format
+        const tokenParts = authHeader.split(' ');
+        if (tokenParts.length !== 2 || tokenParts[0] !== 'Bearer') {
+            throw new Error('Invalid Authorization header format');
+        }
+
+        // Extract the token from the header
+        const token = tokenParts[1];
+
+        // Verify the token and extract user information
         const authUser = jwt.verify(token, process.env.JWT_SECRET);
-        // spara användar info i req
+
+        // Attach user information to the request object
         req.authUser = authUser;
 
         console.log(authUser);
         next();
-    } catch(e) {
-        console.log("JWT token is not valid");
-        res.status(401).send({message: 'Unauthorized', error: e.message});
+    } catch (error) {
+        console.error("JWT token validation error:", error.message);
+        res.status(401).json({ message: 'Unauthorized', error: error.message });
     }
-}
+};
+
---
+++ server.js
@@ -0,0 +1,23 @@
+// server.js
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const authRoutes = require('./routes/authRoutes');
+const { verifyToken } = require('./middleware/auth');
+
+const app = express();
+const port = process.env.PORT || 3030;
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/auth', authRoutes); // Authentication routes
+
+// Protected route example
+/*app.get('/protected', verifyToken, (req, res) => {
+    res.json({ message: 'This route is protected.' });
+});*/
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
